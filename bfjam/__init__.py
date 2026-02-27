f3734b12185c74b5dc52ccb736778a84810d920e
Jesus Amador Mendoza <am378397@uaeh.edu.mx>
2026-02-27 00:04:12 +0000

    Nuevas funciones

--- bfjam/__init__.py
+++ bfjam/__init__.py
@@ -43,3 +43,44 @@ def is_three_colorable(graph):
         if is_proper_coloring(graph, coloring):
             return coloring
     return False
+
+def sum_of_values(values, keys):
+    """
+    Calculates the sum of products of corresponding elements from two lists.
+
+    Args:
+        values (list): A list of numerical values.
+        keys (list): A list of numerical keys (multipliers), typically 0 or 1.
+
+    Returns:
+        float or int: The calculated weighted sum.
+    """
+    total_sum = 0
+    n = len(values)
+    for i in range(n):
+        total_sum += keys[i] * values[i]
+    return total_sum
+
+def knapsack_problem(capacity, goal, weights, profits):
+    """
+    Solves the 0/1 Knapsack Problem to find a combination of items
+    that fits within a given capacity and meets a minimum profit goal.
+
+    Args:
+        capacity (int or float): The maximum weight capacity of the knapsack.
+        goal (int or float): The minimum total profit to achieve.
+        weights (list): A list of weights for each item.
+        profits (list): A list of profits for each item.
+
+    Returns:
+        tuple or bool: A tuple representing the chosen items (1 if chosen, 
+        0 if not)
+        if a valid combination is found, otherwise False.
+    """
+    n = len(weights)
+    combinaciones = itt.product([0, 1], repeat=n)
+    for combinacion in combinaciones:
+        if sum_of_values(weights, combinacion) <= capacity \
+        and sum_of_values(profits, combinacion) >= goal:
+            return combinacion
+    return False
